134f41267aff549aaf62023e442142005b5e541f
mhh-83 <149684294+Devgodot@users.noreply.github.com>
2025-03-10 09:50:07 +0330

    update files

--- src/database.js
+++ src/database.js
@@ -2,7 +2,6 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('data', 'pachim', "haghshenas67", {
     host: 'localhost',
-    port: 3307,
     dialect: 'mysql'
 });
 
