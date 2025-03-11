057a61c7f42b86a4cf9cad976af5d224c2e7dac2
mhh-83 <149684294+Devgodot@users.noreply.github.com>
2025-03-11 11:21:49 +0330

    update files

--- src/database.js
+++ src/database.js
@@ -1,8 +1,12 @@
 const { Sequelize } = require('sequelize');
+const config = require('../config/config.json');
 
-const sequelize = new Sequelize('data', 'pachim', "haghshenas67", {
-    host: 'localhost',
-    dialect: 'mysql'
+const env = process.env.NODE_ENV || 'development';
+const dbConfig = config[env];
+
+const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+  host: dbConfig.host,
+  dialect: dbConfig.dialect
 });
 
 module.exports = sequelize;
