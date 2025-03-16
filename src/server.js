7daa6fa928db5e8db08857cc3578154c34cc5f06
mhh-83 <149684294+Devgodot@users.noreply.github.com>
2025-03-16 04:40:38 +0330

    update files

--- src/server.js
+++ src/server.js
@@ -16,10 +16,6 @@ const config = require('../config/config.json');
 const env = process.env.NODE_ENV || 'development';
 const dbConfig = config[env];
 
-const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
-  host: dbConfig.host,
-  dialect: dbConfig.dialect
-});
 
 const User = require('./models/user');
 const Message = require('./models/message');
